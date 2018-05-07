23a49c0e50cd2d525aea54aff3d7e18653017d9a
Carlos Brandt <chbrandt@pc>
2018-05-07 14:52:51 +0200

    Add setup/package files to *this* package!

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+import versioneer
+
+setup(name='module-2-package',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description='Create Python packages from single modules',
+      packages=['m2l'],
+      install_requires=['click', 'jinja2'],
+      tests_require='pytest',
+      entry_points={
+        'console_scripts': [
+            'm2l = m2l:cli',
+        ],
+      },
+      zip_safe=False,
+      include_package_data=True)
